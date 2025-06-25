69100d7b5d3540ee3e39bd575e491abeb61047f7
Santhoish-Kumar-1812 <santhoshkumarkatroth1812@gmail.com>
2025-06-25 19:46:32 +0530

    hello

---
+++ app.py
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Employee Form</title>
+</head>
+<body>
+  <h2>Add Employee</h2>
+  <form action="submit.php" method="POST">
+    <label for="name">Name:</label><br />
+    <input type="text" id=
+
