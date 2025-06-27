548558513bf5dbdadcc119560faf04443c39f3f7
root <root@ip-172-31-89-124.ec2.internal>
2025-06-27 06:53:30 +0000

    updated

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-!DOCTYPE html>
+DOCTYPE html>
 <html>
 <head>
   <title>Employee Form</title>
 </head>
 <body>
   <h2>Add Employee</h2>
-  <form action="submit.php" method="POST">
-    <label for="name">Name:</label><br />
-    <input type="text" id=
-#santhosh kumar rathod aws engineer
